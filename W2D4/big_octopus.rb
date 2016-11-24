330c5c9c6238ace2057a6f5075f1cf670f32577e
Maria Belgrader <mbelgrader5@gmail.com>
2016-11-24 13:19:18 -0800

    Add methods that run in O(n) time

--- W2D4/big_octopus.rb
+++ W2D4/big_octopus.rb
@@ -5,7 +5,7 @@
 #
 #   array.each do |fish|
 #     array.each do |fish|
-#       longest << fish if fish.length > longest.length
+#       longest = fish if fish.length > longest.length
 #     end
 #   end
 #
@@ -14,13 +14,49 @@
 
 # longest fish in O(n log n) time
 
-def longest_fish(array)
-end
-
 # def longest_fish(array)
+#   array.sort_by(&:length).last
 # end
 
+# longest fish in O(n) time
+
+def longest_fish(array)
+  longest = ''
+  array.each { |fish| longest = fish if fish.length > longest.length}
+  longest
+end
 
 p longest_fish(['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish',
   'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh'])
-# => "fiiiissshhhhhh"
+  # => "fiiiissshhhhhh"
+
+
+# slow dance O(n) time
+
+def slow_dance(dir, tiles)
+  tiles.index(dir)
+end
+
+tiles_array = ["up", "right-up", "right", "right-down", "down",
+  "left-down", "left",  "left-up" ]
+
+p slow_dance("up", tiles_array) #=> 0
+p slow_dance("right-down", tiles_array) #=> 3
+
+def fast_dance(dir, tiles)
+  tiles[dir]
+end
+
+new_tiles_data_structure = {
+  "up" => 0,
+  "right-up" => 1,
+  "right" => 2,
+  "right-down" => 3,
+  "down" => 4,
+  "left-down" => 5,
+  "left" => 6,
+  "left-up" => 7
+}
+
+p fast_dance("up", new_tiles_data_structure) #=> 0
+p fast_dance("right-down", new_tiles_data_structure) #=> 3
