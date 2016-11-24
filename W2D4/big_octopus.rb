a15f79166c415e908a7e47cdbb914bba8cdb0bdf
Maria Belgrader <mbelgrader5@gmail.com>
2016-11-24 13:04:34 -0800

    Add big_octopus

---
+++ W2D4/big_octopus.rb
@@ -0,0 +1,26 @@
+# longest fish in O(n2) time
+
+# def longest_fish(array)
+#   longest = ''
+#
+#   array.each do |fish|
+#     array.each do |fish|
+#       longest << fish if fish.length > longest.length
+#     end
+#   end
+#
+#   longest
+# end
+
+# longest fish in O(n log n) time
+
+def longest_fish(array)
+end
+
+# def longest_fish(array)
+# end
+
+
+p longest_fish(['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish',
+  'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh'])
+# => "fiiiissshhhhhh"
